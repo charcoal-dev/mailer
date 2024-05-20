f43c64f089bdb57efb51423e5ab6e94b2bc40360
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2024-05-20 22:48:31 +0400

    copyright update

--- src/Exception/SmtpClientException.php
+++ src/Exception/SmtpClientException.php
@@ -1,4 +1,15 @@
 <?php
+/*
+ * This file is a part of "charcoal-dev/mailer" package.
+ * https://github.com/charcoal-dev/mailer
+ *
+ * Copyright (c) Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code or visit following link:
+ * https://github.com/charcoal-dev/mailer/blob/master/LICENSE
+ */
+
 declare(strict_types=1);
 
 namespace Charcoal\Mailer\Exception;
