300f89160832a4a86a46d2a4200860d79032d451
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-08-20 11:52:03 +0500

    Refactor: Update namespace references to `Exceptions` and replace file comments with PHPDoc blocks in test files.

--- tests/TemplatingTest.php
+++ tests/TemplatingTest.php
@@ -1,13 +1,7 @@
 <?php
-/*
- * This file is a part of "charcoal-dev/mailer" package.
- * https://github.com/charcoal-dev/mailer
- *
- * Copyright (c) Furqan A. Siddiqui <hello@furqansiddiqui.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code or visit following link:
- * https://github.com/charcoal-dev/mailer/blob/master/LICENSE
+/**
+ * Part of the "charcoal-dev/mailer" package.
+ * @link https://github.com/charcoal-dev/mailer
  */
 
 declare(strict_types=1);
@@ -19,7 +13,7 @@ class TemplatingTest extends \PHPUnit\Framework\TestCase
 {
     /**
      * @return void
-     * @throws \Charcoal\Mailer\Exception\TemplatingException
+     * @throws \Charcoal\Mailer\Exceptions\TemplatingException
      */
     public function testBodyLoad(): void
     {
@@ -30,18 +24,18 @@ class TemplatingTest extends \PHPUnit\Framework\TestCase
 
     /**
      * @return void
-     * @throws \Charcoal\Mailer\Exception\TemplatingException
+     * @throws \Charcoal\Mailer\Exceptions\TemplatingException
      */
     public function testNonExistentBody(): void
     {
-        $this->expectException(\Charcoal\Mailer\Exception\TemplatingException::class);
+        $this->expectException(\Charcoal\Mailer\Exceptions\TemplatingException::class);
         $this->getTemplatingEngine()->getBody("this_doesnt_exist");
     }
 
     /**
      * @return void
-     * @throws \Charcoal\Mailer\Exception\DataBindException
-     * @throws \Charcoal\Mailer\Exception\TemplatingException
+     * @throws \Charcoal\Mailer\Exceptions\DataBindException
+     * @throws \Charcoal\Mailer\Exceptions\TemplatingException
      */
     public function testTemplateEmail(): void
     {
